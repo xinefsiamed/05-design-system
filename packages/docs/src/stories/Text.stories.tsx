c7a0000d2f552c3ae6b3bc146102aa66a6c2ab28
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2023-08-27 21:19:53 -0300

    feat: adding component  text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis doloremque magnam quidem vero placeat consequatur quaerat unde corporis nobis. Saepe animi incidunt blanditiis veritatis perferendis sunt similique non libero impedit.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
